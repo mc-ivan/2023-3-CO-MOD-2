8f8b7d17949180d0bebddb3c2c750ea9a1bf92b3
Ivan Mamani <ivan.mamani@whitecupsolutions.com>
2023-04-27 00:55:53 -0400

    Dyno Base project

---
+++ dino_runner/components/game.py
@@ -0,0 +1,49 @@
+import pygame
+
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        pygame.display.set_icon(ICON)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.playing = False
+        self.game_speed = 20
+        self.x_pos_bg = 0
+        self.y_pos_bg = 380
+
+    def run(self):
+        # Game loop: events - update - draw
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.quit()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.clock.tick(FPS)
+        self.screen.fill((255, 255, 255))
+        self.draw_background()
+        pygame.display.update()
+        pygame.display.flip()
+
+    def draw_background(self):
+        image_width = BG.get_width()
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+        if self.x_pos_bg <= -image_width:
+            self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+            self.x_pos_bg = 0
+        self.x_pos_bg -= self.game_speed
---
+++ dino_runner/utils/constants.py
@@ -0,0 +1,72 @@
+import pygame
+import os
+
+# Global Constants
+TITLE = "Chrome Dino Runner"
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+FPS = 30
+IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
+
+# Assets Constants
+ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
+
+RUNNING = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+]
+
+RUNNING_SHIELD = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1Shield.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+]
+
+RUNNING_HAMMER = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+]
+
+JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJump.png"))
+JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpShield.png"))
+JUMPING_HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpHammer.png"))
+
+DUCKING = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+]
+
+DUCKING_SHIELD = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Shield.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+]
+
+DUCKING_HAMMER = [
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+]
+
+SMALL_CACTUS = [
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
+]
+LARGE_CACTUS = [
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus3.png")),
+]
+
+BIRD = [
+    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2.png")),
+]
+
+CLOUD = pygame.image.load(os.path.join(IMG_DIR, 'Other/Cloud.png'))
+SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
+HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png'))
+
+BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
+
+HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
+
+DEFAULT_TYPE = "default"
