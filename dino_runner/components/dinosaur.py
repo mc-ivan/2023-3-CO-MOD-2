e3c6d3d75c4e791b2da33976b656c6a31e5f3df3
Ivan Mamani <ivan.mamani@whitecupsolutions.com>
2023-05-01 19:19:22 -0400

    CLASS-2 Homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,13 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur:
     X_POS = 80
     Y_POS = 310
     JUMP_SPEED = 8.5
+    Y_POS_DUCK = 340
 
     def __init__(self):
         self.image = RUNNING[0]
@@ -17,26 +18,33 @@ class Dinosaur:
         self.step_index = 0
         self.dino_jump = False
         self.jump_speed = self.JUMP_SPEED
-        
+        self.dino_duck = False
 
-    def update(self, user_imput):
-        # si el dino esta corriendo es True
+    def update(self, user_input):
+        # Actions control
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
-        
-        # colocar en cero a step_index caundo es mayor a 10
-        if self.step_index > 10:
-            self.step_index = 0
+        elif self.dino_duck:
+            self.duck()
 
-        # Actulizar variable si el dino salta o corre basado en la entrada del teclado
-        if user_imput[pygame.K_UP] and not self.dino_jump:
+        # Attributes control based on user input
+        if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
+
+        # colocar en cero a step_index caundo es mayor a 10
+        if self.step_index > 10:
+            self.step_index = 0
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
@@ -57,3 +65,10 @@ class Dinosaur:
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
